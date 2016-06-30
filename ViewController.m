8b7e8344ec9e1b39841805bae93397e8fcb09487
Bin <Bin@lsbdeMacBook-Pro.local>
2016-06-30 14:19:56 +0800

    new version

--- ViewController.m
+++ ViewController.m
@@ -22,8 +22,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIColor *color = [UIColor colorWithRed:236/255.0 green:95/255.0 blue:103/255.0 alpha:1.0];
     // Do any additional setup after loading the view, typically from a nib.
      _password = [[SBPasswordTextField alloc] initWithFrame:CGRectMake(40, 66, 240, 40)];
+   // _password.secureTextEntry = NO;
+    _password.septaLineWidth = 3;
+    _password.septaLineColor = color;
+    _password.dotFillColor = color;
+    _password.dotRadius = 10;
     [self.view addSubview:_password];
 //   // self.view.backgroundColor = [UIColor grayColor];
     
